bf8300d6d6b8f9d87762dfd3f4c42070fd279774
lilxak <zakben45@gmail.com>
2019-12-12 20:28:01 +0100

    Optimize connexion with yts
    Get movie's page once to get both the score and the category

--- ytsdata/getdata.py
+++ ytsdata/getdata.py
@@ -5,33 +5,33 @@ from bs4 import BeautifulSoup
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s-%(levelname)s-%(message)s')
 # logging.disable(logging.CRITICAL)
 
-def get_movie_rating(movie_title_selector, yts_parser):
-    """ Returns a movie's rating from its title's selector """
+def get_movie_page(movie_title_selector, yts_parser):
+    """ Returns a movie's page """
     movie_page_link = yts_parser.select(movie_title_selector)[0]['href']
     try:
         movie_page = requests.get(movie_page_link)
     except:
         print('Cannot access to yts.lt. Please check your connexion.')
         return None
-    else:
+    return movie_page
+
+def get_movie_rating(movie_page):
+    """ Returns a movie's rating from its page """
+    if movie_page:
         movie_rating_selector = 'div.rating-row:nth-child(2) > span:nth-child(2)'
         parser = BeautifulSoup(movie_page.text, 'html.parser')
         rating = parser.select(movie_rating_selector)[0].text
         return rating
+    return None
 
-def get_movie_category(movie_title_selector, yts_parser):
+def get_movie_category(movie_page):
     """ Returns a movie's category from its title's selector """
-    movie_page_link = yts_parser.select(movie_title_selector)[0]['href']
-    try:
-        movie_page = requests.get(movie_page_link)
-    except:
-        print('Cannot access to yts.lt. Please check your connexion.')
-        return None
-    else:
+    if movie_page:
         movie_category_selector = 'div.hidden-xs:nth-child(1) > h2:nth-child(3)'
         parser = BeautifulSoup(movie_page.text, 'html.parser')
         category = parser.select(movie_category_selector)[0].text
         return category       
+    return None
 
 def get_movie_data():
     """ Returns a list of latest YTS movies added """
@@ -47,8 +47,9 @@ def get_movie_data():
         for i in [2,3]:
             for j in range(1,5):
                 movie_title = parser.select(movie_title_selector.format(i,j))[0].text
-                movie_score = get_movie_rating(movie_title_selector.format(i,j), parser)
-                movie_category = get_movie_category(movie_title_selector.format(i,j), parser)
+                movie_page = get_movie_page(movie_title_selector.format(i,j),parser)
+                movie_score = get_movie_rating(movie_page)
+                movie_category = get_movie_category(movie_page)
                 movies[movie_title] = [float(movie_score), movie_category]
     return movies
 
