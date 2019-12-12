429b4e0eef1452ccdd133355b5e735fdcfac20c2
lilxak <zakben45@gmail.com>
2019-12-12 17:52:39 +0100

    Store Categories with Titles and Ratings
    Add new column 'Category' to the movies_data Dataframe

--- ytsdata/storedata.py
+++ ytsdata/storedata.py
@@ -10,16 +10,16 @@ def read_existing_data():
     try:
         movies_data = pd.read_csv('movies.csv', index_col=0)
     except:
-        movies_data = pd.DataFrame(columns=['Movie','Rating'])
+        movies_data = pd.DataFrame(columns=['Movie','Rating','Category'])
     return movies_data
 
 def write_data():
     """ Writes the new entries into the 'movies.csv' file """
     movies_data = read_existing_data()
     new_movies = gd.get_movie_data()
-    for movie, rating in new_movies.items():
+    for movie, rating_category in new_movies.items():
         if movie not in movies_data['Movie'].values:
-            new_row = [movie, rating]
+            new_row = [movie, rating_category[0], rating_category[1]]
             movies_data.loc[-1] = new_row
             movies_data.index += 1
     movies_data.to_csv('movies.csv')
