b83855873ebde6ec648906db495330f89f87fdb8
lilxak <zakben45@gmail.com>
2019-12-12 13:12:25 +0100

    Store movies' data in a dataframe

--- ytsdata/storedata.py
+++ ytsdata/storedata.py
@@ -8,9 +8,9 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s-%(levelname)s-%(mes
 def read_existing_data():
     """ Returns a pandas Series either containing previous entries or empty """
     try:
-        movies_data = pd.read_csv('movies.csv')
+        movies_data = pd.read_csv('movies.csv', index_col=0)
     except:
-        movies_data = pd.Series()
+        movies_data = pd.DataFrame(columns=['Movie','Rating'])
     return movies_data
 
 def write_data():
@@ -18,7 +18,10 @@ def write_data():
     movies_data = read_existing_data()
     new_movies = gd.get_movie_titles()
     for movie, rating in new_movies.items():
-        movies_data[movie] = rating
+        if movie not in movies_data['Movie'].values:
+            new_row = [movie, rating]
+            movies_data.loc[-1] = new_row
+            movies_data.index += 1
     movies_data.to_csv('movies.csv')
 
 logging.debug(write_data())
