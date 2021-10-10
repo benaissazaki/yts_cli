7cc10212795b79c9fe9f49f9a44bf1c62faebe22
Zaki Benaissa <zakben45@gmail.com>
2021-10-10 11:37:44 +0100

    Add comments

--- ytsdata/__main__.py
+++ ytsdata/__main__.py
@@ -6,6 +6,8 @@ if __name__ == '__main__':
     QUERY = ''
 
     while True:
+        # Example: Keep asking the user to search
+        # for a movie and display results
         QUERY = input('\n* Search for a movie: ')
         results = yts_api.list_movies(query_term=QUERY)['movies']
         for r in results:
